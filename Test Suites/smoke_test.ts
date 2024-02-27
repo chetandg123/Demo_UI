9d94ac3a79660151c1f3a14d9e48f5e33109d417
chetan <chetan@latitude-7480>
2024-02-27 14:34:50 +0530

    added global variables

--- Test Suites/smoke_test.ts
+++ Test Suites/smoke_test.ts
@@ -17,6 +17,12 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/first_testcase</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3eb58e01-dad5-4423-998e-649c44367747</variableId>
+      </variableLink>
    </testCaseLink>
    <testCaseLink>
       <guid>12ecbf4d-d124-4489-ad89-32df6a2ef5cb</guid>
@@ -24,6 +30,12 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/second_testcase</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f1be7a50-607a-47e6-a439-b84e01217bfe</variableId>
+      </variableLink>
    </testCaseLink>
    <testCaseLink>
       <guid>ae2a98fd-88a0-462d-836f-2ddd56939e84</guid>
@@ -31,5 +43,11 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/third_testcase</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f4f0a7cc-cc2c-402f-b0cf-351c551c7b51</variableId>
+      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
